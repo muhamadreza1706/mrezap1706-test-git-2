917f855e2bfec80f8dfe59c37c2ecb19ede417ae
muhamadreza1706 <mrezap1706@gmail.com>
2024-01-24 11:01:26 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -7,5 +7,10 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <ul>
+        <li>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum expedita sit eos consectetur, nemo excepturi! Ipsa voluptatibus dolores fugiat libero dolorem veniam eius nihil, delectus asperiores. Nisi maiores commodi provident.</p>
+        </li>
+    </ul>
 </body>
 </html>
